0275eb207c0cbe25551a39284f0330cf1010d071
ice_bpadua <ice_bpadua@uolinc.com>
2020-10-04 23:58:29 -0300

    swagger-fix

--- partner/src/main/java/io/github/bapadua/demo/partner/config/SwaggerConfig.java
+++ partner/src/main/java/io/github/bapadua/demo/partner/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package io.github.bapadua.demo.partner.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
